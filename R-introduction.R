aec894bb64c2a2634457c68dc71a099eb1c75cba
Christina Koch <christinakconnect@gmail.com>
2017-09-29 16:23:11 -0500

    Create R-introduction.R

---
+++ R-introduction.R
@@ -0,0 +1,27 @@
+## saving values to names
+age <- 2017 - 1955
+age
+
+weight_kg <- 55
+weight_lb <- weight_kg * 2.2
+
+# These are bad variable names
+x <- 55
+y <- x * 2.2
+
+z <- 5
+z * 5
+## Other data types
+name <- "Christina"
+is_tall <- TRUE
+
+## using functions
+area <- 25
+sqrt(area)
+
+pi <- 3.14159
+round(x=pi,digits=2)
+
+tens <- seq(from=10,to=100,by=10)
+tens[2]
+sqrt(tens)
